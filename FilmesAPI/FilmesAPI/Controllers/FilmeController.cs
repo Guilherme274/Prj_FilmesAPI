73c6223c5b31c5cc67e4fe12e99eca8e2634c4d2
Guilherme Paixão dos Santos <111549138+Guilherme274@users.noreply.github.com>
2023-02-14 09:27:31 -0300

    Add files via upload

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,47 @@
+﻿
+
+using FilmesAPI.Data;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult AddMovie([FromBody] Filme filme)
+    {
+
+        _context.Filmes.Add(filme);
+        
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, 
+                filme);
+        
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0,
+        [FromQuery] int take = 50)
+    {
+        return _context.Filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
